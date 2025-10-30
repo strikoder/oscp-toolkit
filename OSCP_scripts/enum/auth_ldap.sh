9d40ca9f3cf90a29c59c216280d9c207100cc009
Amin Yaßin <ameen.yaseen.98@gmail.com>
2025-10-30 18:02:30 +0100

    Add output directory information to auth_ldap.sh

--- OSCP_scripts/enum/auth_ldap.sh
+++ OSCP_scripts/enum/auth_ldap.sh
@@ -110,3 +110,4 @@ echo "==============================================================="
 echo "    - Full dump: $OUT_MAIN"
 echo "    - Info ctx:  $OUT_INFO"
 echo "    - UAC=...32: $OUT_PWDNR"
+echo "    - For full results check: $OUTDIR/
